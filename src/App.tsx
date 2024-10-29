eeae9c8261b20555e86183334615208482970b2d
Andrew Keturi <adketuri@gmail.com>
2024-10-29 08:35:19 -0700

    Add vite + websockets starter

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
+function App() {
+  const socketUrl = "ws://localhost:3000";
+  const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
+
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage]);
+
+  // const handleClickSendMessage = useCallback(() => sendMessage("Hello"), []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
+  return (
+    <div>
+      <button
+        onClick={() => sendMessage(JSON.stringify({ message: "hello world" }))}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        hello
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
+      <ul>
+        {messageHistory.map((message, idx) => (
+          <span key={idx}>{message ? message.data : null}</span>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
